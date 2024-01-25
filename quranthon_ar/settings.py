f2b8c660bb438bd8b8dee295680e670839acfef2
Ahmed Amine Touahria <ahmedtouahria2001@gmail.com>
2024-01-25 23:12:40 +0100

    add decouple

--- quranthon_ar/settings.py
+++ quranthon_ar/settings.py
@@ -1,23 +1,17 @@
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@&b(4@rr_4)5kx(6$&7-8=pbdx!@ftv8ttv#f4vnizs$gdx1t5'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',True)
 
 ALLOWED_HOSTS = []
 
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -25,6 +19,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ar'
 ]
 
 MIDDLEWARE = [
